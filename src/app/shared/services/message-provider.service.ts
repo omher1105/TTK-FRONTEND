fdbefa274d4323b61922bd1d3f9d02bcc7cf1f00
Omar Hermamdez <omar.hertnandez@atomchat.io>
2022-07-09 04:40:18 -0500

    feat(offers): add module offers

---
+++ src/app/shared/services/message-provider.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageProviderService {
+
+  constructor(public dialog: MatDialog) {
+  }
+
+  showModal(component, config): MatDialogRef<any> {
+    return this.dialog.open(component, config);
+  }
+
+  showModalPromise(component, dialogData, width?, disableClose?): any {
+    return this.dialog.open(component, {
+      width: (width ? width : '800px'),
+      disableClose: (disableClose ? disableClose : false), data: dialogData
+    }).afterClosed().toPromise();
+  }
+}
