012064ceeb2cce708a37f14ddb365e53192ea78f
omar hernandez <omar.reyhs@gmail.com>
2022-08-11 23:25:25 -0500

    feat: add mockups admision

--- src/app/modules/admin/admision/containers/entrevistas/entrevistas.component.ts
+++ src/app/modules/admin/admision/containers/entrevistas/entrevistas.component.ts
@@ -1,14 +1,34 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {AdmisionService} from '../../admision.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {Oferta} from '../../admision.interface';
+import {BehaviorSubject, merge, Subject, switchMap} from 'rxjs';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {MessageProviderService} from '../../../../../shared/services/message-provider.service';
+import {PostulacionesService} from '../postulaciones/postulaciones.service';
+import {FormUtils} from '../../../../../shared/utils/form.utils';
 
 @Component({
   selector: 'app-entrevistas',
   templateUrl: './entrevistas.component.html',
   styleUrls: ['./entrevistas.component.scss']
 })
-export class EntrevistasComponent implements OnInit, AfterViewInit {
+export class EntrevistasComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  dataSource: Oferta[] = [];
+  displayedColumns: string[] = ['imagen', 'informacion', 'responsable', 'estado', 'actions'];
+
+  count = 0;
+
+  changesSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  unsubscribe: Subject<void> = new Subject<void>();
 
   constructor(
+      private _ngxSpinner: NgxSpinnerService,
+      private _messageProviderService: MessageProviderService,
+      private _postulacionService: PostulacionesService,
       private _admisionService: AdmisionService,
   ) {
     this._admisionService.title.next('Entrevistas');
@@ -19,6 +39,33 @@ export class EntrevistasComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
 
+    this.paginator._intl.itemsPerPageLabel = 'Items por página.';
+
+    this.initPagination();
+  }
+
+  initPagination(): void {
+    merge(this.paginator.page, this.changesSubject, this._postulacionService.eventFilters)
+        .pipe(
+            switchMap(() => {
+              this._ngxSpinner.show();
+              const rawValue = this._postulacionService.eventFilters.value;
+              const filters = rawValue ? FormUtils.deleteKeysNullInObject(rawValue) : null;
+              const queryParamsByPaginator = {...filters} as any;
+              queryParamsByPaginator.limit = this.paginator.pageSize;
+              queryParamsByPaginator.offset = queryParamsByPaginator.limit * this.paginator.pageIndex;
+              return this._postulacionService.get(queryParamsByPaginator);
+            })
+        ).subscribe((response) => {
+      this._ngxSpinner.hide();
+      this.count = response.count;
+      this.dataSource = response.results;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
--- src/app/modules/admin/admision/containers/examen-medico/examen-medico.component.ts
+++ src/app/modules/admin/admision/containers/examen-medico/examen-medico.component.ts
@@ -1,14 +1,34 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {AdmisionService} from '../../admision.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {Oferta} from '../../admision.interface';
+import {BehaviorSubject, merge, Subject, switchMap} from 'rxjs';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {MessageProviderService} from '../../../../../shared/services/message-provider.service';
+import {PostulacionesService} from '../postulaciones/postulaciones.service';
+import {FormUtils} from '../../../../../shared/utils/form.utils';
 
 @Component({
   selector: 'app-examen-medico',
   templateUrl: './examen-medico.component.html',
   styleUrls: ['./examen-medico.component.scss']
 })
-export class ExamenMedicoComponent implements OnInit, AfterViewInit {
+export class ExamenMedicoComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  dataSource: Oferta[] = [];
+  displayedColumns: string[] = ['imagen', 'informacion', 'responsable', 'estado', 'actions'];
+
+  count = 0;
+
+  changesSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  unsubscribe: Subject<void> = new Subject<void>();
 
   constructor(
+      private _ngxSpinner: NgxSpinnerService,
+      private _messageProviderService: MessageProviderService,
+      private _postulacionService: PostulacionesService,
       private _admisionService: AdmisionService,
   ) {
     this._admisionService.title.next('Examen medico');
@@ -18,6 +38,34 @@ export class ExamenMedicoComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+
+    this.paginator._intl.itemsPerPageLabel = 'Items por página.';
+
+    this.initPagination();
+  }
+
+  initPagination(): void {
+    merge(this.paginator.page, this.changesSubject, this._postulacionService.eventFilters)
+        .pipe(
+            switchMap(() => {
+              this._ngxSpinner.show();
+              const rawValue = this._postulacionService.eventFilters.value;
+              const filters = rawValue ? FormUtils.deleteKeysNullInObject(rawValue) : null;
+              const queryParamsByPaginator = {...filters} as any;
+              queryParamsByPaginator.limit = this.paginator.pageSize;
+              queryParamsByPaginator.offset = queryParamsByPaginator.limit * this.paginator.pageIndex;
+              return this._postulacionService.get(queryParamsByPaginator);
+            })
+        ).subscribe((response) => {
+      this._ngxSpinner.hide();
+      this.count = response.count;
+      this.dataSource = response.results;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
